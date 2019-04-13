f2d9f0aa2febd15f9e9974724771a4f44dda1aee
James Rosado <dev@twmsllc.com>
2019-04-12 23:11:16 -0400

    adjust path to comodocreds.json

--- classes/Comodo.py
+++ classes/Comodo.py
@@ -15,7 +15,7 @@ class Comodo():
         #Comodo login Credentials stored in a seperate Credentials file
         #JSON format {"loginName": "<login username>", "loginPassword": "<login password>"}
         path = os.path.dirname(os.path.realpath(__file__))
-        with open(path + '/comodocreds.json', 'r') as comodoCreds:
+        with open(path + '/../comodocreds.json', 'r') as comodoCreds:
                 self.creds = json.load(comodoCreds)
         	
         self.headers = {
