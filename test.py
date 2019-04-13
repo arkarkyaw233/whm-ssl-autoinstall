931d1f1b4891c0e44ab57bf35607a16e30604d06
James Rosado <dev@twmsllc.com>
2019-04-13 07:57:38 -0400

    testing - comodo

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 #! /usr/bin/python3.6
-#from classes.Commands import Commands
+from classes.Commands import Commands
 from classes.Comodo import Comodo
 #from classes.Dom import Domain
 #from classes.Log import Log
 #from classes.whm import WHM
 
 #Test Commands class
-#input_args = Commands()
-com = Comodo('Y')
+input_args = Commands()
+com = Comodo(input_args.input_args['testssl'])
 #test
