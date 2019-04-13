36913c543705185ab3788c53d57aefbee26af2c1
James Rosado <dev@twmsllc.com>
2019-04-13 07:40:18 -0400

    testing

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 #! /usr/bin/python3.6
 from classes.Commands import Commands
-from classes.Comodo import Comodo
-from classes.Dom import Domain
-from classes.Log import Log
-from classes.whm import WHM
+#from classes.Comodo import Comodo
+#from classes.Dom import Domain
+#from classes.Log import Log
+#from classes.whm import WHM
 
 #Test Commands class
 #input_args = Commands()
