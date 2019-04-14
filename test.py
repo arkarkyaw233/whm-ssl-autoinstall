85dda2bb24c4ff803480bf738dc2beab51a10995
James Rosado <dev@twmsllc.com>
2019-04-13 21:42:44 -0400

    testing

--- test.py
+++ test.py
@@ -9,34 +9,49 @@ path = os.path.dirname(os.path.realpath(__file__))
 #from classes.Log import Log
 #from classes.whm import WHM
 
-#
-#Test Commands class
-#
+"""
+Test Commands class
+"""
 
 #input_args = Commands()
 
-#
-#Test Comodo class
-#
+"""
+Test Comodo class
+"""
 
 com = Comodo('Y')
 
-#Test Comodo.setDcvMethod
+"""
+    Test Comodo.setDcvMethod
+"""
+
 #print('DCV Method before Comodo.setDcvMethod: ' + com.args['dcvMethod'])
 #com.setDcvMethod('HTTP_CSR_HASH')
 #print('DCV Method after Comodo.setDcvMethod: ' + com.args['dcvMethod'])
 
-#Test Comodo.request Methods
+"""
+    Test Comodo.get_csr_hashes
+"""
 
-#Test Comodo.get_csr_hashes
 #with open(path + '/test/testCsrData.json', 'r') as testCsrData:
 #    csr_data = json.load(testCsrData)
 #print('Test Comodo.get_csr_hashes ::')
 #print(com.get_csr_hashes(csr_data['csr']))
 
-#Test Comodo.orderSsl
-with open(path + '/test/testCsrData.json', 'r') as testCsrData:
-    csr_data = json.load(testCsrData)
-print('Test Comodo.orderSsl\n SSL Order No:: ')
-orderSsl = com.orderSsl(csr_data['csr'])
-print(orderSsl[1])
+"""
+    Test Comodo.orderSsl
+"""
+
+#with open(path + '/test/testCsrData.json', 'r') as testCsrData:
+#    csr_data = json.load(testCsrData)
+#print('Test Comodo.orderSsl\n SSL Order No:: ')
+#orderSsl = com.orderSsl(csr_data['csr'])
+#print(orderSsl[1])
+
+"""
+    Test Comodo.collectSsl
+"""
+
+with open(path + '/test/testSslOrder.json', 'r') as testSslOrder:
+    testSslOrder = json.load(testSslOrder)
+print(testSslOrder[1])
