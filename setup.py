d3351c431ef7fd1f25b23b5b050af5fe42230d28
James Rosado <dev@twmsllc.com>
2019-04-19 02:13:03 -0400

    built package installer

---
+++ setup.py
@@ -0,0 +1,43 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
+setuptools.setup(
+
+     name='whmsslinstall',  
+
+     version='0.1',
+
+     scripts=['sslinstall'] ,
+
+     author="James Rosado",
+
+     author_email="dev@twmsllc.com",
+
+     description= ("An Application to order and "
+        "install Comodo DV SSL Certificates"
+     ),
+
+     long_description=long_description,
+
+    long_description_content_type="text/markdown",
+
+     url="https://github.com/twmsllc/whm-ssl-autoinstall",
+
+     packages=setuptools.find_packages(),
+
+     classifiers=[
+
+         "Programming Language :: Python :: 3",
+
+         "License :: GNU General Public License",
+
+         "Operating System :: CentOS 6+",
+
+         "CPanel :: EasyApache4+ "
+
+     ],
+
+ )
